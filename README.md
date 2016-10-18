a7174ae16fad694bfd7d23dd74c5662ad594863e
James Graham <james10graham@gmail.com>
2016-10-18 22:14:07 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Sort Words
+
+Sorts a series of words delimited by dots or spaces.
+
+`cmd-shift-s` to toggle.
+
+![CSS Class Sort](sort-words.gif)
---
+++ spec/sort-words-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SortWordsView from '../lib/sort-words-view';
+
+describe('SortWordsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
